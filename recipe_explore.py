6d7a038d9ab520cdc8c99b6db2b90a03a29a5cf4
NehaNishikant <31891939+NehaNishikant@users.noreply.github.com>
2022-11-10 13:46:32 -0500

    added cheese

--- recipe_explore.py
+++ recipe_explore.py
@@ -4,7 +4,10 @@ import pandas
 with open('data/raw/ingr_map.pkl', 'rb') as f:
     data = pk.load(f)
 
-print(type(data))
-data.head().to_csv("data/recipe_head.csv") 
+# print(type(data))
+# data.head().to_csv("data/recipe_head.csv") 
+
+cheese = data.query('"cheese" in processed')
+cheese.to_csv("data/cheese.csv") 
 
 
